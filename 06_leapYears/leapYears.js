9adf9d75668e867f36c1aa7d4c3dbf3012da132c
Bart Zweigoron <bzweigoron@gmail.com>
2022-11-07 16:58:49 -0500

    submit assignment

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let leapYear = true;
+    
+    if (year % 4 != 0) {
+        leapYear = false;
+    }
+
+    if (year % 100 == 0 && year % 400 != 0) {
+        leapYear = false;
+    }
+
+    return leapYear;
 
 };
+
+console.log(leapYears(2000));
 
 // Do not edit below this line
 module.exports = leapYears;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,18 @@
-const ftoc = function() {
+const ftoc = function(temp_f) {
+  return roundToOne((temp_f - 32) / 1.8);
 
 };
 
-const ctof = function() {
-
+const ctof = function(temp_c) {
+  return roundToOne((temp_c *9/5) +32);
 };
+
+function roundToOne(num) {
+  return Math.round(num * 10) / 10
+}
+
+console.log(ctof(212));
+console.log(ftoc(22))
 
 // Do not edit below this line
 module.exports = {
