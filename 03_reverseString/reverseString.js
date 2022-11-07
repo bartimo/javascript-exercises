27cd55c59ea651c2cdd15c718a6bf016a68b725b
Bart Zweigoron <bzweigoron@gmail.com>
2022-11-07 10:52:35 -0500

    submit solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+   return Array.from(str).reverse().join("") //convert string to array, reverse it, then join with no separator.
 };
 
 // Do not edit below this line
+
 module.exports = reverseString;
