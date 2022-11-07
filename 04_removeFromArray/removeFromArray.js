1a0cc4ec7245560c6325bd1d3536f213f0a41481
Bart Zweigoron <bzweigoron@gmail.com>
2022-11-07 11:53:51 -0500

    4 of 3 tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,) {
+    for (let i=1; i < arguments.length; i++) {
 
+            let retVal = arr.splice(arr.indexOf(arguments[i]),1)
+        
+    }
+    return arr;
 };
+
+
+/* debug code
+let testArray = [1,2,3,4,5]
+let t = removeFromArray(testArray,2,4)
+console.log(t)
+*/
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
