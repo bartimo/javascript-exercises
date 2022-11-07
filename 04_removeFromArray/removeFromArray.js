5cae3964feef01b924b523838fa0350617e25576
Bart Zweigoron <bzweigoron@gmail.com>
2022-11-07 12:11:16 -0500

    Submit Assignment

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,18 +1,19 @@
 const removeFromArray = function(arr,) {
     for (let i=1; i < arguments.length; i++) {
-
-            let retVal = arr.splice(arr.indexOf(arguments[i]),1)
-        
+        if (arr[arr.indexOf(arguments[i])] === arguments[i]) {
+            let retVal = arr.splice(arr.indexOf(arguments[i]),1) 
+        }
     }
     return arr;
 };
 
 
 /* debug code
-let testArray = [1,2,3,4,5]
-let t = removeFromArray(testArray,2,4)
-console.log(t)
+let testArray = [1,2,3,4,5];
+let t = removeFromArray(testArray,2,4,100);
+console.log(t);
 */
+
 
 
 
