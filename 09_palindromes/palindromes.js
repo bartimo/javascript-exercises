0415f84b5122a45d943b0ba2b23f44079373ff8b
Bart Zweigoron <bzweigoron@gmail.com>
2022-11-19 00:48:02 -0500

    submit palindromes assignment

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function(a) {
+    let str = a.toUpperCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replaceAll(' ','').split("");
+    len = str.length
+    
+    for(let x = 0; x < len; x++) {
+        console.log(`${str[x]} = ${str[len-x-1]}`)
+        if(str[x] !== str[len-x-1]) {
+            return false
+        }
+    }
+    return true;
 };
 
+console.log(palindromes("racecara"));
 // Do not edit below this line
 module.exports = palindromes;
