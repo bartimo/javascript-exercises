d54a05efc94a9ed2561985d4a486766399e0f3ea
Bart Zweigoron <bzweigoron@gmail.com>
2022-11-19 09:52:18 -0500

    submit getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,29 @@
-const getTheTitles = function() {
+/*
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+  */
 
+const getTheTitles = function(bookObj) {
+    return titles = bookObj.map(book => {return book.title});
+    
+   /* loop method
+    let titles = [];
+    for(i = 0; i < bookObj.length; i++) {
+        titles.push(bookObj[i].title);
+    }
+    return titles
+    */
 };
+
+//console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
