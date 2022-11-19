689214c3774201ef6d9c0ba3fc01e6b4ebfb7407
Bart Zweigoron <bzweigoron@gmail.com>
2022-11-19 09:38:15 -0500

    submit fibonacci assignment

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {  
+    if(num > 0) {
+        let fib = [0,1]; //starting array
+        for(let x = 1; x < num; x++){
+            fib.push(fib[x-1] + fib[x]);
+        }
+        //console.log(fib);
+        return(fib[num]);
+        console.log(fib);
+    } else {
+        return "OOPS";
+    }
+  
 };
+
+console.log(fibonacci("8"));
 
 // Do not edit below this line
 module.exports = fibonacci;
