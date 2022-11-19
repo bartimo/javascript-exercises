282bc77787e778654144b2404a6efe4b57f64b7c
Bart Zweigoron <bzweigoron@gmail.com>
2022-11-19 10:39:21 -0500

    submit findTheOldest assignment

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
 
-};
+
+const findTheOldest = function(person) {
+    return person.reduce((oldest,current) => {
+        if(!oldest.yearOfDeath) {
+            oldest.yearOfDeath = new Date().getFullYear();
+        }
+        if(!current.yearOfDeath) {
+            current.yearOfDeath = new Date().getFullYear();
+        }
+        return (current.yearOfDeath - current.yearOfBirth >  oldest.yearOfDeath - oldest.yearOfBirth ? current : oldest) })
+    };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
